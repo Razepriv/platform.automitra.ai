8c1a1fbf6159596893c4706eeea970284d20432a
Razeen Shaheed <raze.priv@gmail.com>
2025-12-25 06:11:54 +0530

    chore: Remove unused ScrollArea import

--- client/src/components/AgentFormDialog.tsx
+++ client/src/components/AgentFormDialog.tsx
@@ -14,7 +14,6 @@ import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { 
   FileText, 
   Cpu, 
